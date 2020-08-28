dc0f70c93a57ace2cf839ed4c8e91600aebb653d
activenoob <yusufkehinde11@gmail.com>
2020-08-28 21:10:52 +0100

    default team changed

--- src/components/useDarkMode.js
+++ src/components/useDarkMode.js
@@ -3,7 +3,7 @@ import {
     useState
 } from 'react';
 export const useDarkMode = () => {
-    const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState('light');
     const [mountedComponent, setMountedComponent] = useState(false)
     const setMode = mode => {
         window.localStorage.setItem('theme', mode)
