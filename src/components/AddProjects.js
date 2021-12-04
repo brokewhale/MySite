65584233ea0a9ba4fba9d6750033dd0beff9dafb
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2021-12-05 00:06:36 +0100

    fix: screen size

--- src/components/AddProjects.js
+++ src/components/AddProjects.js
@@ -1,57 +1,48 @@
-import React from 'react';
-import Project from './Project';
-import Homepage from '../img/Homepage.png';
-import dark from '../img/dark.png';
+import React from "react";
+import Project from "./Project";
+import Homepage from "../img/Homepage.png";
+import dark from "../img/dark.png";
 // import quote from '../img/quote.png';
 // import chat from '../img/chat.png';
-import weather from '../img/weather.png';
-import { FaReact, FaGithub } from 'react-icons/fa';
-import commitspy from '../img/commitspy.png';
-
-
-
+import weather from "../img/weather.png";
+import { FaReact, FaGithub } from "react-icons/fa";
+import commitspy from "../img/commitspy.png";
 
 function AddProjects() {
-    return (
-        <div className='addproject' id='pro'>
-            <p className='feat'>Featured Projects. <span className='more'><a href="https://github.com/CodingMage">more on <FaGithub className='darkmode_icon' /></a></span></p>
-            <div className="addproject__grid">
+  return (
+    <div className="addproject" id="pro">
+      <p className="feat">
+        Featured Projects.{" "}
+        <span className="more">
+          <a href="https://github.com/brokewhale">
+            more on <FaGithub className="darkmode_icon" />
+          </a>
+        </span>
+      </p>
+      <div className="addproject__grid">
+        <a href="https://myhotelk.netlify.app">
+          <Project image={Homepage} name="MyHotel" />
+        </a>
+        <a href="https://rankedevdarklandingpage.netlify.app">
+          <Project image={dark} name="Dark" />
+        </a>
+        <a href="https://commitspy.netlify.app/">
+          <Project
+            image={commitspy}
+            name={"CommitSpy"}
+            imgincase={<FaReact className="imgincase" />}
+          />
+        </a>
 
-                <a href="https://myhotelk.netlify.app">
-
-                    <Project
-                        image={Homepage}
-                        name='MyHotel'
-                    />
-                </a>
-                <a href="https://rankedevdarklandingpage.netlify.app">
-
-                    <Project
-                        image={dark}
-                        name='Dark'
-                    />
-                </a>
-                <a href="https://commitspy.netlify.app/">
-
-                    <Project
-                        image={commitspy}
-                        name={"CommitSpy"}
-                        imgincase={<FaReact className='imgincase' />}
-                    />
-                </a>
-
-                <a href="https://reactmobileweatherapp.netlify.app/">
-
-                    <Project
-                        image={weather}
-                        name='Mobile weather App'
-                        imgincase={<FaReact className='imgincase' />}
-
-                    />
-                </a>
-            </div>
-        </div>
-
-    )
+        <a href="https://reactmobileweatherapp.netlify.app/">
+          <Project
+            image={weather}
+            name="Mobile weather App"
+            imgincase={<FaReact className="imgincase" />}
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
-export default AddProjects
+export default AddProjects;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import { FaLinkedinIn, FaTwitter, FaGithub, FaCodepen, FaPhone, FaCopyright } from 'react-icons/fa';
-
+import React from "react";
+import {
+  FaLinkedinIn,
+  FaTwitter,
+  FaGithub,
+  FaCodepen,
+  FaPhone,
+  FaCopyright,
+} from "react-icons/fa";
 
 function Footer() {
-    return (
-        <footer className='footer'>
-            <div className="footer__icon">
-                <a href="www.linkedin.com/in/yusufkehinde11" className='icon'><FaLinkedinIn className='icon' /></a>
-                <a href="https://twitter.com/Kool_Kage" className='icon'><FaTwitter className='icon' /></a>
-                <a href="mailto:yusufkehinde11@gmail.com" className='icon'><FaPhone className='icon' /></a>
-                <a href="https://github.com/CodingMage" className='icon'><FaGithub className='icon' /></a>
-                <a href="https://codepen.io/rankeddev" className='icon'><FaCodepen className='icon' /></a>
-
-
-
-            </div>
-            <div className="footer__copy">
-                <span> <FaCopyright className='copy' />2020 YUSUF KEHINDE</span>
-                <a href='mailto:yusufkehinde11@gmail.com' target="_blank" rel="noopener noreferrer" className='email-link'> yusufkehinde11@gmail.com</a>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <div className="footer__icon">
+        <a href="www.linkedin.com/in/yusufkehinde11" className="icon">
+          <FaLinkedinIn className="icon" />
+        </a>
+        <a href="https://twitter.com/Kool_Kage" className="icon">
+          <FaTwitter className="icon" />
+        </a>
+        <a href="mailto:yusufkehinde11@gmail.com" className="icon">
+          <FaPhone className="icon" />
+        </a>
+        <a href="https://github.com/CodingMage" className="icon">
+          <FaGithub className="icon" />
+        </a>
+        <a href="https://codepen.io/rankeddev" className="icon">
+          <FaCodepen className="icon" />
+        </a>
+      </div>
+      <div className="footer__copy">
+        <span>
+          {" "}
+          <FaCopyright className="copy" />
+          {new Date().getFullYear()} YUSUF KEHINDE
+        </span>
+        <a
+          href="mailto:yusufkehinde11@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="email-link"
+        >
+          {" "}
+          yusufkehinde11@gmail.com
+        </a>
+      </div>
+    </footer>
+  );
 }
-export default Footer
+export default Footer;
